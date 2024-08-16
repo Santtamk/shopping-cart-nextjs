e85fc523c658b876918e44753fdaa9a9e0905ed2
Santam Kumai <santtamk@gmail.com>
2024-08-16 18:02:40 +0530

    local storage added

--- src/app/CartContext.js
+++ src/app/CartContext.js
@@ -1,49 +1,96 @@
 "use client";
-import { createContext, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Store = createContext("");
 
 const StoreProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedCart = localStorage.getItem("cart");
+      if (savedCart) {
+        try {
+          setCart(JSON.parse(savedCart));
+        } catch (error) {
+          console.error("Failed to parse cart from localStorage", error);
+          localStorage.removeItem("cart");
+        }
+      }
+    }
+  }, []);
+
+  const updateLocalStorage = (cart) => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  };
 
   const addToCart = (item) => {
-    // setCart([...cart, item]);
     setCart((prevCart) => {
       const existingItem = prevCart.find((cartItem) => cartItem.id === item.id);
+
       if (existingItem) {
         //if the item already exists, do not add it again
-        //removeonbuild
         // console.log("item already exists in cart:", item);
-        //add snackbar, that says"item already in cart, you can visit your cart to change quantity"
         toast.info("Item already in cart. Visit your cart to change quantity.");
         return prevCart;
       } else {
+        // console.log("item added to cart", item, new Date().toISOString());
+        const updatedCart = [...prevCart, { ...item, quantity: 1 }];
         toast.success("Item added to cart");
-        return [...prevCart, { ...item, quantity: 1 }];
+        updateLocalStorage(updatedCart);
+        return updatedCart;
       }
     });
   };
 
-  //   const removeFromCart = (itemId) => {
-  //     setCart(cart.filter((item) => item.id !== itemId));
-  //   };
+  const removeFromCart = (itemId) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.filter((item) => item.id !== itemId);
+      toast.warning("Removed from cart");
+      updateLocalStorage(updatedCart);
+      return updatedCart;
+    });
+  };
 
-  //   const adjustQuantity = (itemId, amount) => {
-  //     setCart(
-  //       cart.map((item) =>
-  //         item.id === itemId
-  //           ? { ...item, quantity: item.quantity + amount }
-  //           : item
-  //       )
-  //     );
-  //   };
+  const increment = (itemId) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      updateLocalStorage(updatedCart);
+      return updatedCart;
+    });
+  };
 
+  const decrement = (itemId) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart
+        .map((item) =>
+          item.id === itemId && item.quantity > 0
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => {
+          if (item.id === itemId && item.quantity === 0) {
+            toast.warning("Removed from cart");
+            return false;
+          }
+          return true;
+        });
+      updateLocalStorage(updatedCart);
+      return updatedCart;
+    });
+  };
+
+  
   return (
-    <Store.Provider value={{ cart, addToCart }}>
+    <Store.Provider
+      value={{ cart, addToCart, removeFromCart, increment, decrement }}
+    >
       {children}
-      <ToastContainer />
     </Store.Provider>
   );
 };
--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,38 +1,86 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Store } from "../CartContext";
 
 const CartPage = () => {
-  const { cart } = useContext(Store);
+  //context
+  const { cart, increment, removeFromCart, decrement } = useContext(Store);
+
+  //subtotal
+  let subTotal = cart
+    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+    .toFixed(2);
+
+  //discount
+  let discount = (subTotal * 0.1).toFixed(2);
+
+  //total
+  let total = (subTotal - discount).toFixed(2);
 
   return (
     <div>
-      <div>Yours items in cart</div>
       {cart.length === 0 ? (
         <div className="flex items-center justify-center font-bold decoration-blue underline">
           Your cart is empty
         </div>
       ) : (
         <>
+          <div className="bg-white text-xl">Your items in cart</div>
           {cart.map((item) => (
-            <div key={item.id} className="bg-white flex p-4 gap-3">
+            <div key={item.id} className="bg-white flex p-4 gap-4">
               <img
                 src={item.image}
                 alt={item.title}
                 className="mix-blend-multiply object-scale-down w-20 h-20"
               />
-              <div className="sm:w-4/5">
+              <div className="sm:w-3/5 flex flex-col gap-4">
                 <div className="overflow-hidden truncate text-black md:text-wrap font-semibold	text-wrap">
                   {item.title}
                 </div>
-                <div>
-                  <button>-</button>
-                  <div>{item.quantity}</div>
-                  <button>+</button>
+                <div className="flex gap-3 ">
+                  <button
+                    className="border p-4 rounded-xl bg-gray border-blue hover:bg-white shadow-2xl"
+                    onClick={() => decrement(item.id)}
+                  >
+                    -
+                  </button>
+                  <div className="border p-4 rounded-xl bg-gray border-blue">
+                    {item.quantity}
+                  </div>
+                  <button
+                    className="border p-4 rounded-xl bg-gray border-blue hover:bg-white shadow-2xl"
+                    onClick={() => increment(item.id)}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div>
+                <div className="sm:w-1/5 flex justify-center items-center text-center text-black font-semibold flex-col gap-5">
+                  <div className="decoration-blue underline">${item.price}</div>
+                  <button
+                    className=" border-blue border p-2 text-black text-rose shadow-2xl rounded-md hover:bg-blue hover:text-white "
+                    onClick={() => removeFromCart(item.id)}
+                  >
+                    Remove Item
+                  </button>
                 </div>
               </div>
             </div>
           ))}
+          <div className="bg-white border flex flex-col p-5 gap-4">
+            <div className="flex gap-5">
+              Sub-total:{" "}
+              <div className="decoration-blue underline">${subTotal}</div>
+            </div>
+            <div>Extra discount of 10% on ${subTotal}</div>
+            <div className="flex gap-5">
+              Total: <div className="decoration-blue underline">${total}</div>
+            </div>
+            <div className="border text-center border-blue text-black text-rose shadow-2xl rounded-md hover:bg-blue cursor-pointer hover:text-white">
+              Checkout
+            </div>
+          </div>
         </>
       )}
     </div>
